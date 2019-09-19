89b79bea84c4964fface547ac6cc547b25e6b1a0
Jason Barr <jason@jasonsbarr.com>
2019-09-18 22:02:11 -0500

    Initialize useState

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+  const [score, setScore] = useState({ lions: 0, tigers: 0 });
 
   return (
     <div className="container">
